dfa7af98c0bf4a613b4f93bff8be0b7b7d6a1b45
Benjy Weinberger <benjyw@gmail.com>
2020-03-10 19:19:41 -0700

    Flesh out the repo with some sample code.
    Also a pants_from_sources script, for convenience.

---
+++ src/python/helloworld/main.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Pants project contributors.
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from colors import green
+from helloworld.greeting import Greeter
+
+
+def say_hello():
+    greeter = Greeter()
+    sentence = greeter.greet("world")
+    print(green(sentence))
+
+
+if __name__ == "__main__":
+    say_hello()
---
+++ src/python/util/lang.py
@@ -0,0 +1,27 @@
+# Copyright 2020 Pants project contributors.
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+import random
+
+from translate import Translator
+from util.resources import get_lines
+
+
+class LanguageTranslator:
+    class UnknownLanguage(Exception):
+        pass
+
+    def __init__(self):
+        self._langs = get_lines(__name__, "langs.txt")
+
+    def translate(self, lang: str, phrase: str) -> str:
+        if lang not in self._langs:
+            raise self.UnknownLanguage(lang)
+        translator = Translator(lang)
+        return translator.translate(phrase)
+
+    def translate_to_random_language(self, phrase: str) -> str:
+        return self.translate(self._pick_random_language(), phrase)
+
+    def _pick_random_language(self):
+        return random.choice(self._langs)
