7c17c5d003adc2f25d56159e84361c051a1c131e
Benjy Weinberger <benjyw@gmail.com>
2020-03-23 13:28:24 -0700

    Python 2 code examples. (#5)

---
+++ helloworld/greet_py2/greeting_py2.py
@@ -0,0 +1,7 @@
+# Copyright 2020 Pants project contributors.
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+
+def greet_py2(name):
+    exec "x = 'hello, {}'.format(name)"
+    return x
