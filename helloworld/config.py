14e467ce54e3e918fb2321aa2d79974397558adb
Benjy Weinberger <benjyw@gmail.com>
2020-07-29 23:17:26 -0700

    Fix the setup-py part of the example. (#30)
    Required moving the .proto files to be
    below the exported target (in the filesystem
    sense).
    
    If we support setting a source root on
    generated protos in the future, we can
    modify this further to demo that.

--- helloworld/config.py
+++ helloworld/config.py
@@ -1,8 +1,8 @@
 # Copyright 2020 Pants project contributors.
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from helloworld.protos.config_pb2 import Config
 from helloworld.util.config_loader import load_config_from_json
+from helloworld.util.proto.config_pb2 import Config
 
 
 def load_config() -> Config:
